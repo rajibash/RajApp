6c5638ea40d90a080bd84a2ee44ecc21a3990067
rajibash <168216370+rajibash@users.noreply.github.com>
2024-05-06 20:30:53 +0100

    Add files via upload

--- NewApp.py
+++ NewApp.py
@@ -14,22 +14,26 @@ st.set_page_config(page_title="Daily Hotel Sales",
 st.subheader("Daily Sales Record")
 
 
+
+
 with st.form(key="form", clear_on_submit=True):
-    date = st.number_input("Enter today's date)
     roomNumber = st.text_input('Enter the room number')
     amount = st.number_input('Enter the Tamount paid')
     add_btn = st.form_submit_button("Add", type="primary")
+    date = st.number_input("Enter todays date")
 
 Sales = 0
 
     # Event handler
-def SalesCalculator(date, roomNumber, amount):
+def SalesCalculator(roomNumber, amount, date):
         if roomNumber and amount:
             Sales =+ amount
             Sales = round(Sales,2)
             print(f"The total sales for {date} is {Sales}")
             return f"The total sales for {date} is {Sales}"
             
+
+
 if add_btn:
         display = SalesCalculator(roomNumber=roomNumber, amount=amount, date=date)
         st.subheader(display)
