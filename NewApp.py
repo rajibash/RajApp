12dc4d26d947987b1fcc01680582fe83bfeef0c2
rajibash <168216370+rajibash@users.noreply.github.com>
2024-05-06 21:41:39 +0100

    Update NewApp.py

--- NewApp.py
+++ NewApp.py
@@ -4,37 +4,33 @@ import pandas as pd
 
 import calendar
 
-yy = 2024
+from datetime import date
 
+todays_date = date.today()
 
 st.set_page_config(page_title="Daily Hotel Sales",
                    page_icon="🖥")
 
 
-st.subheader("Daily Sales Record")
-
-
-
+st.subheader("Hotel Sales Record")
 
 with st.form(key="form", clear_on_submit=True):
+    date = todays_date = date.today()
     roomNumber = st.text_input('Enter the room number')
-    amount = st.number_input('Enter the Tamount paid')
+    amount = st.number_input('Enter the amount paid')
     add_btn = st.form_submit_button("Add", type="primary")
-    date = st.number_input("Enter todays date")
 
 Sales = 0
 
     # Event handler
-def SalesCalculator(roomNumber, amount, date):
+def SalesCalculator(roomNumber, amount):
         if roomNumber and amount:
             Sales =+ amount
             Sales = round(Sales,2)
-            print(f"The total sales for {date} is {Sales}")
-            return f"The total sales for {date} is {Sales}"
+            print(f"The total sales for {todays_date} is {Sales}")
+            return f"The total sales for {todays_date} is {Sales}"
             
-
-
 if add_btn:
-        display = SalesCalculator(roomNumber=roomNumber, amount=amount, date=date)
+        display = SalesCalculator(roomNumber=roomNumber, amount=amount)
         st.subheader(display)
 
